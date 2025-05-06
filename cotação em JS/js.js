c31352ae944e3d3fa0e17bdb450ea422692da6f5
Rafael <rafael.cusco14@gmail.com>
2025-05-06 20:24:31 -0300

    Delete cotação em JS directory

--- cotação em JS/js.js
+++
@@ -1,6 +0,0 @@
-let valordolar=prompt("Digite o valor do dólar, por favor");
-let cotacaod=5.71;
-alert("O valor do dólar em reais é: R$ " + (valordolar*cotacaod).toFixed(2));  
-let valorWon=prompt("Digite o valor do Won, por favor");
-let cotacaow=0.0043;
-alert("O valor do Won em reais é: R$ " + (valorWon*cotacaow).toFixed(2));  
